6732d310211fb38f6f77bbece5291d83b17fb577
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-03 17:01:18 +0300

    Created Reservation Entity

---
+++ src/main/java/com/example/demo/entity/Reservation.java
@@ -0,0 +1,36 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "reservation",schema = "house_management")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "start_date")
+    private Long id;
+
+    @Column(name = "start_date")
+    private String startDate;
+
+    @Column(name = "end_date")
+    private String endDate;
+
+    @Column(name = "total_price")
+    private Double totalPrice;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "listing_id",nullable = false)
+    private Listing listing;
+}
