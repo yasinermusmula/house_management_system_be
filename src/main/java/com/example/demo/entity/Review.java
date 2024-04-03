d8bec83b7e1fd8a3bbea170e2e7d3cc461e44812
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-03 17:01:05 +0300

    Created Review Entity

---
+++ src/main/java/com/example/demo/entity/Review.java
@@ -0,0 +1,39 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "review")
+    private String reviewText;
+
+    @Column(name = "rating")
+    private Double rating;
+
+    @Column(name = "review_date")
+    private LocalDate reviewDate;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "listing_id",nullable = false)
+    private Listing listing;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+}
