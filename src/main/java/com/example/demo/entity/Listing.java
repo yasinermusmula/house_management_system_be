b515fb9b1b13775e111af62ca9d84244a85faa91
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-03 17:01:49 +0300

    Created Listing Entity

---
+++ src/main/java/com/example/demo/entity/Listing.java
@@ -0,0 +1,44 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "listing",schema = "house_management")
+public class Listing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @ElementCollection
+    @CollectionTable(name = "listing_photos",
+            joinColumns = @JoinColumn(name = "listing_id"))
+    @Column(name = "photo_url")
+    private List<String> photoUrls;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "property_type")
+    private Property property;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "listing",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Review> reviews;
+}
