5f503d40091c60a0581476be3da74193b7ad16b1
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-04 15:05:36 +0300

    UserServiceImpl class created and UserService method implemented

---
+++ src/main/java/com/example/demo/util/service/UserServiceImpl.java
@@ -0,0 +1,64 @@
+package com.example.demo.util.service;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.util.UserDtoConvertion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private UserRepository userRepository;
+
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDto save(User user) {
+        //TODO Make validation here and write exceptions
+        userRepository.save(user);
+        return UserDtoConvertion.convertUser(user);
+    }
+
+    @Override
+    public UserDto findById(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()){
+            User user = optionalUser.get();
+            return UserDtoConvertion.convertUser(user);
+        }
+        //TODO Make validation here and write exceptions
+        return null;
+    }
+
+    @Override
+    public UserDto delete(Long id) {
+        UserDto userDto = findById(id);
+        if (userDto != null){
+            userRepository.deleteById(id);
+            return userDto;
+        }
+        //TODO Make validation here and write exceptions
+        return null;
+    }
+
+    @Override
+    public List<UserDto> findAllUser() {
+        List<User> userList = userRepository.findAll();
+        return UserDtoConvertion.convertList(userList);
+    }
+
+    @Override
+    public List<UserDto> findUsersEmail(String email) {
+        List<User> userList = userRepository.findUserByEmail(email);
+        return UserDtoConvertion.convertList(userList);
+    }
+}
