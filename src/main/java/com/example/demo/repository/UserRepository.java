a64e25c00f2e99e96c1cfcc5af986957d236fd1c
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-04 14:37:55 +0300

    Custom query added to UserRepository

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    List<User> findUserByEmail(String email);
 }
