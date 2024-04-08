2e7c8006d4e68df174677840d783ad9ee217cebb
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-08 21:06:51 +0300

    HouseListingServiceImpl class created and CRUD methods implemented

---
+++ src/main/java/com/example/demo/service/HouseListingServiceImpl.java
@@ -0,0 +1,63 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.HouseListingDto;
+import com.example.demo.entity.HouseListing;
+import com.example.demo.entity.User;
+import com.example.demo.repository.HouseListingRepository;
+import com.example.demo.util.HouseListingConvertion;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class HouseListingServiceImpl implements HouseListingService{
+
+    private HouseListingRepository houseListingRepository;
+    private UserService userService;
+
+    @Override
+    public HouseListingDto save(HouseListing houseListing, Long userId) {
+        User user = userService.findByOriginal(userId);
+        if (user != null){
+            HouseListing newHouseListing = new HouseListing();
+            newHouseListing.setTitle(houseListing.getTitle());
+            newHouseListing.setDescription(houseListing.getDescription());
+            newHouseListing.setPhotoUrls(houseListing.getPhotoUrls());
+            newHouseListing.setProperty(houseListing.getProperty());
+            newHouseListing.setUser(user);
+            newHouseListing.setReviews(houseListing.getReviews());
+            return HouseListingConvertion.convertHouseListing(newHouseListing);
+        }
+        //TODO Make Exceptions here
+        return null;
+    }
+
+    @Override
+    public List<HouseListingDto> findAll() {
+        List<HouseListing> houseListings = houseListingRepository.findAll();
+        return HouseListingConvertion.convertList(houseListings);
+    }
+
+    @Override
+    public List<HouseListingDto> findByUser(Long userId) {
+        List<HouseListing> houseListings = houseListingRepository.findHouseListingByUserId(userId);
+        return HouseListingConvertion.convertList(houseListings);
+    }
+
+    @Override
+    public HouseListingDto delete(Long userId) {
+        HouseListing houseListing = findByOriginal(userId);
+        houseListingRepository.delete(houseListing);
+        return HouseListingConvertion.convertHouseListing(houseListing);
+    }
+
+    @Override
+    public HouseListing findByOriginal(Long id) {
+        return houseListingRepository.findById(id).orElse(null);
+    }
+}
