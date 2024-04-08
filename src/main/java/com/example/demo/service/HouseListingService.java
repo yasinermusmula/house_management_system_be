38401728d524319c832da5dc8348ee0a7e582183
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-08 21:06:16 +0300

    Methods implemented for using in HouseListingServiceImpl class

--- src/main/java/com/example/demo/service/HouseListingService.java
+++ src/main/java/com/example/demo/service/HouseListingService.java
@@ -1,8 +1,18 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.HouseListingDto;
 import com.example.demo.entity.HouseListing;
 
-public interface HouseListingService {
-    HouseListing save (HouseListing houseListing);
+import java.util.List;
 
+public interface HouseListingService {
+    HouseListingDto save (HouseListing houseListing, Long userId);
+
+    List<HouseListingDto> findAll();
+
+    List<HouseListingDto> findByUser(Long userId);
+
+    HouseListingDto delete(Long userId);
+
+    HouseListing findByOriginal(Long id);
 }
