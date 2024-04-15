0c3e4670539ef11e1e1e161b85f6dc506165a88a
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-15 21:26:24 +0300

    RoleRepository interface created

---
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+    @Query("SELECT r FROM Role r WHERE r.authority = :authority")
+    Optional<Role> findByAuthority(String authority);
+}
