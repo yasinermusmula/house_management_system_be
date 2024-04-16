52d98b9f3f712758d9695f25e1d2d627cbd5c8d6
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-04-16 18:38:13 +0300

    renamed from RegisterUSerDto

--- src/main/java/com/example/demo/dto/RegisterUSerDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.dto;
-
-public record RegisterUSerDto(String email, String name, String password, String role, String sirname) {
-}
---
+++ src/main/java/com/example/demo/dto/RegisterUserDto.java
@@ -0,0 +1,4 @@
+package com.example.demo.dto;
+
+public record RegisterUserDto(String email, String name, String password, String role, String sirname) {
+}
